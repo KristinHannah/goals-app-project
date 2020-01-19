279f252cbbfed5b9ddca32841e99c2dec08f6a4f
KristinHannah <kristinhannahk@gmail.com>
2020-01-19 11:23:41 -0500

    building out controller

--- goals-app-backend/app/controllers/api/v1/goals_controller.rb
+++ goals-app-backend/app/controllers/api/v1/goals_controller.rb
@@ -1,6 +1,40 @@
-class GoalsController < ApplicationController
+class Api::v1::GoalsController < ApplicationController
+
   def index
     @goals = Goal.all 
-    render json: @goals 
+    render json: @goals, status: 200 
   end
+
+  def show 
+    @goal = Goal.find(params[:id])
+
+    render json: @goal, status: 200 
+  end 
+
+  def create 
+    @goal = Goal.create(goal_params)
+    #note.new, if note.save, render json 
+  end 
+
+  def update 
+    @goal = Goal.find(params[:id])
+    @goal.update(goal_params)
+    render json: @goal, status: 200 
+    #if note.find, then note.update, etc. 
+  end 
+
+  def destroy 
+    @goal = Goal.find(params[:id])
+    @goal.delete 
+
+    render json: {goalId: @goal.id}
+  end 
+
+  
+  private 
+
+  def goal_params 
+    params.require(:goal).permit(:name, :category, :user_id)
+  end 
+
 end
