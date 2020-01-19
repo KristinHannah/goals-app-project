32b23d3a4f8c58cb34630981f53193a7c0a0ea94
KristinHannah <kristinhannahk@gmail.com>
2020-01-19 15:50:21 -0500

    refining components

--- goal-app-frontend/src/components/goals.js
+++ goal-app-frontend/src/components/goals.js
@@ -2,14 +2,20 @@ class Goals {
     constructor() {
         this.goals = []
         this.adapter = new GoalsAdapter()
-        this.bindEventListeners()
+        // this.bindEventListeners()
         this.fetchAndLoadGoals()
     }
 
     fetchAndLoadGoals() {
         this.adapter.getGoals().then(goals => {
-            console.log(goals)
+            goals.forEach(goal => this.goals.push(goal))
         })
+            .then(() => { this.render() })
+    }
+
+    render() {
+        const goalsContainer = document.getElementById('goals-container')
+        goalsContainer.innerHTML = ' my goals here '
     }
 
 }
--- goal-app-frontend/src/index.js
+++ goal-app-frontend/src/index.js
@@ -1,24 +1,24 @@
 
 const app = new App()
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM content has loaded")
-  let url = "http://localhost:3000/api/v1/goals/index"
-  fetch(url)
-    .then(resp => resp.json())
-    .then(data => data.forEach(goal => {
-      displayRow(goal)
-    }))
-})
+//document.addEventListener("DOMContentLoaded", () => {
+ // console.log("DOM content has loaded")
+ // let url = "http://localhost:3000/api/v1/goals/index"
+ // fetch(url)
+ //   .then(resp => resp.json())
+ //   .then(data => data.forEach(goal => {
+ //     displayRow(goal)
+ //   }))
+// })
 
-function displayRow(row) {
-  const table = document.querySelector("#goal-table")
-  table.innerHTML +=
-    `
-  <tr>
-    <td>${goal.name}</td>
-    <td>${goal.category}</td>
-    <td>${goal.user_id}</td>
-  </tr>
-  `
-}
+// function displayRow(row) {
+ // const table = document.querySelector("#goal-table")
+ //  table.innerHTML +=
+ //    `
+ //  <tr>
+ //    <td>${goal.name}</td>
+ //    <td>${goal.category}</td>
+  //   <td>${goal.user_id}</td>
+ //  </tr>
+ //  `
+//  }
