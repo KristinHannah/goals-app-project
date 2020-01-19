0acad4f9f5d6500dee30fc895dc763d26faaa216
KristinHannah <kristinhannahk@gmail.com>
2020-01-19 15:13:52 -0500

    linking backend and frontend

--- goals-app-backend/db/seeds.rb
+++ goals-app-backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(name: "kkay", password:"password")
+user1.goals.create([{name: "eat well", category: "health and fitness"}, {name: "live sustainably", category: 'social good'}])
